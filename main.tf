eac4555493ad248fab1c0d55da899e55b999980a
JoeFlex80 <88585159+JoeFlex80@users.noreply.github.com>
2021-09-14 19:31:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "my_security_group" {
 }
 
 # Create AWS ec2 instance
-resource "aws_instance" "my1stInstance" {
+resource "aws_instance" "my1st-Instance" {
   ami           = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
@@ -46,9 +46,9 @@ resource "aws_instance" "my1stInstance" {
 }
 
 # Create Elastic IP address
-resource "aws_eip" "my1stInstance" {
+resource "aws_eip" "my1st-Instance" {
   vpc      = true
-  instance = aws_instance.my1stInstance.id
+  instance = aws_instance.my1st-Instance.id
 tags= {
     Name = "my_elastic_ip"
   }
