e741000ec5b7f3b39e9fef46ba4b55d3f4944e7a
JoeFlex80 <88585159+JoeFlex80@users.noreply.github.com>
2021-09-14 20:27:13 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "security_group" {
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
-  default     = "my1st-instance"
+  default     = "myfirstinstance"
 }
 
 variable "ami_id" {
