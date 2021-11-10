c8a7ad7aec18dd1f6f960eebdbd7a04c978390a0
JoeFlex80 <88585159+JoeFlex80@users.noreply.github.com>
2021-11-09 23:36:57 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "Rich-EC2-Key"
+  default     =  "JNKNS_KP"
 }
 
 variable "instance_type" {
